041aceb15109e6dbd0c63702b031c2d0b4f6ff96
Khalid <csewebmail@gmail.com>
2017-12-30 09:42:43 +0600

    Admin Side Templates

---
+++ resources/views/user/app.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    @include('user.layout.head')
+</head>
+
+<body>
+
+    <!-- Navigation -->
+        @include('user.layout/header')
+    <!-- Main Content -->
+        @section('main_content')
+            @show
+    <!-- Footer -->
+    
+        @include('user.layout.footer')
+    </body>
+
+</html>
